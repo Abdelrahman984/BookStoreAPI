2eb8259609f8ec8ca1532b21b02b0f95d908f84e
Abdelrahman Alaa <rafeeq220044@gmail.com>
2025-02-14 04:50:48 +0200

    Add endpoints for managing customers and admins
    Updated CustomerController with new endpoints for customer management, including validation and error handling. Re-added AddCustomerDTO and EditCustomerDTO with validation. Added AdminController with endpoints for admin management. Added ChangePasswordDTO, AddAdminDTO, EditAdminDTO, SelectAllAdminsDTO, and SelectAllCustomersDTO with validation.

--- BookStoreAPI/Controllers/CustomerController.cs
+++ BookStoreAPI/Controllers/CustomerController.cs
@@ -1,11 +1,12 @@
-﻿using BookStoreAPI.DTOs.CustomerDTO;
+﻿using BookStoreAPI.DTOs;
+using BookStoreAPI.DTOs.CustomerDTO;
 using BookStoreAPI.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookStoreAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class CustomerController : ControllerBase
     {
@@ -16,45 +17,145 @@ namespace BookStoreAPI.Controllers
             this.userManager = userManager;
             this.roleManager = roleManager;
         }
+
+        [HttpGet]
+        public IActionResult SelecAllCustomers()
+        {
+            var customers = userManager.GetUsersInRoleAsync("Customer").Result.OfType<Customer>().ToList();
+            if (!customers.Any()) return NotFound();
+            List<SelectAllCustomersDTO> customersDTO = new List<SelectAllCustomersDTO>();
+            foreach (var cust in customers)
+            {
+                SelectAllCustomersDTO cDTO = new SelectAllCustomersDTO()
+                {
+                    Id = cust.Id,
+                    UserName = cust.UserName,
+                    FullName = cust.FullName,
+                    Email = cust.Email,
+                    PhoneNumber = cust.PhoneNumber,
+                    Address = cust.Address
+                };
+                customersDTO.Add(cDTO);
+            }
+            return Ok(customersDTO);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult SelectCustomerById(string id)
+        {
+            var cust = (Customer)userManager.GetUsersInRoleAsync("Customer").Result.FirstOrDefault(c => c.Id == id);
+            if (cust == null) return NotFound();
+            SelectAllCustomersDTO cDTO = new SelectAllCustomersDTO()
+            {
+                Id = cust.Id,
+                UserName = cust.UserName,
+                FullName = cust.FullName,
+                Email = cust.Email,
+                PhoneNumber = cust.PhoneNumber,
+                Address = cust.Address
+            };
+            return Ok(cDTO);
+        }
+
+
         [HttpPost]
         public IActionResult AddCustomer(AddCustomerDTO customerDTO)
         {
-            var cust = new Customer()
+            if (ModelState.IsValid)
             {
-                UserName = customerDTO.UserName,
-                FullName = customerDTO.FullName,
-                Email = customerDTO.Email,
-                PhoneNumber = customerDTO.PhoneNumber,
-                Address = customerDTO.Address
-            };
-            var result = userManager.CreateAsync(cust, customerDTO.Password).Result;
-
-            if (result.Succeeded)
-            {
-                IdentityRole _role = roleManager.FindByNameAsync("Customer").Result;
-                if (_role != null)
+                var cust = new Customer()
                 {
-                    IdentityResult roleResult = userManager.AddToRoleAsync(cust, _role.Name).Result;
-                    if (roleResult.Succeeded)
-                        return Ok();
+                    UserName = customerDTO.UserName,
+                    FullName = customerDTO.FullName,
+                    Email = customerDTO.Email,
+                    PhoneNumber = customerDTO.PhoneNumber,
+                    Address = customerDTO.Address
+                };
+                var result = userManager.CreateAsync(cust, customerDTO.Password).Result;
+
+                if (result.Succeeded)
+                {
+                    IdentityRole _role = roleManager.FindByNameAsync("Customer").Result;
+                    if (_role != null)
+                    {
+                        IdentityResult roleResult = userManager.AddToRoleAsync(cust, _role.Name).Result;
+                        if (roleResult.Succeeded)
+                            return Ok();
+                        else
+                            return BadRequest(roleResult.Errors);
+                    }
                     else
-                        return BadRequest(roleResult.Errors);
+                    {
+                        return BadRequest("Role not found");
+                    }
                 }
                 else
                 {
-                    return BadRequest("Role not found");
+                    return BadRequest(result.Errors);
                 }
             }
             else
             {
-                return BadRequest(result.Errors);
+                return BadRequest(ModelState);
             }
         }
-        [HttpGet]
-        public IActionResult SelecAll()
+
+        [HttpPut]
+        public IActionResult EditCustomer(EditCustomerDTO customerDTO)
         {
-            var customers = userManager.Users.ToList();
-            return Ok(customers);
+            if (ModelState.IsValid)
+            {
+                var cust = (Customer)userManager.FindByIdAsync(customerDTO.Id).Result;
+                if (cust != null)
+                {
+                    cust.FullName = customerDTO.FullName;
+                    cust.Email = customerDTO.Email;
+                    cust.PhoneNumber = customerDTO.PhoneNumber;
+                    cust.Address = customerDTO.Address;
+                    cust.UserName = customerDTO.UserName;
+
+                    var result = userManager.UpdateAsync(cust).Result;
+                    if (result.Succeeded)
+                        return NoContent();
+                    else
+                        return BadRequest(result.Errors);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
         }
+
+        [HttpPut]
+        public IActionResult ChangeCustomerPassword(ChangePasswordDTO changePasswordDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                var cust = (Customer)userManager.FindByIdAsync(changePasswordDTO.Id).Result;
+                if (cust != null)
+                {
+                    var result = userManager.ChangePasswordAsync(cust, changePasswordDTO.CurrentPassword, changePasswordDTO.NewPassword).Result;
+                    if (result.Succeeded)
+                        return NoContent();
+                    else
+                        return BadRequest(result.Errors);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+
     }
 }
